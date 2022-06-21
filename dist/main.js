ef58e2474a76f4dd9e8965c141ff274d84e6a53b
GuigzPC <guillaume.dufau.pro@gmail.com>
2022-06-21 14:54:10 +0200

    :sparkles: Start and end port + press space to start

--- dist/main.js
+++ dist/main.js
@@ -1,22 +1,67 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+// Canvas settings
 canvas.width = 0.6 * innerWidth
 canvas.height = canvas.width / 16 * 9
 
-// console.log(canvas)
+// State of game
+let started = false
+
+function readTextFile(file) {
+     let allText
+     let rawFile = new XMLHttpRequest()
+     rawFile.open("GET", file, false)
+     rawFile.onreadystatechange = function () {
+          if (rawFile.readyState === 4) {
+               if (rawFile.status === 200 || rawFile.status == 0) {
+                    allText = rawFile.responseText
+               }
+          }
+     }
+     rawFile.send(null)
+     return allText
+}
+
+console.log(readTextFile("../map1.txt").split('\r\n'))
+
+let maps = JSON.parse(readTextFile("../maps.json"))
+
+const mapSettings = maps[0]
+const map = readTextFile(mapSettings.map).split('\r\n')
+
+const startPort = document.querySelector('.canvas-container .start')
+startPort.style.left = mapSettings.startPort.x + "px"
+startPort.style.top = mapSettings.startPort.y + "px"
+startPort.style.display = "block"
+startPort.style.transform = `rotate(${mapSettings.startPort.rotation}deg)`
+
+const endPort = document.querySelector('.canvas-container .end')
+endPort.style.right = mapSettings.endPort.x + "px"
+endPort.style.bottom = mapSettings.endPort.y + "px"
+endPort.style.display = "block"
+endPort.style.transform = `rotate(${mapSettings.endPort.rotation}deg)`
+
+const xStart = mapSettings.startPort.x + startPort.offsetWidth / 2
+const yStart = mapSettings.startPort.y + startPort.offsetHeight / 2
+console.log(xStart, yStart)
+
+function drawMap() {
+}
 
 class Player {
      constructor() {
           this.position = {
-               x: 20,
-               y: 20
+               x: xStart,
+               y: yStart
           }
+          this.speed = mapSettings.speed
           this.velocity = {
-               x: 0,
-               y: 1
+               x: mapSettings.velocity.x,
+               y: mapSettings.velocity.y
           }
-          
+
+
           this.path = []
           this.smallRadius = 2
           this.bigRadius = 6
@@ -36,18 +81,18 @@ class Player {
           c.fill(bigCircle)
      }
 
-     update() {          
+     update() {
           // TODO - Lose function and detect BBs
-          if (this.position.x - this.bigRadius / 2 + this.velocity.x > 0 && this.position.x + this.bigRadius / 2 + this.velocity.x < canvas.width) {
+          if (this.position.x - this.bigRadius + this.velocity.x > 0 && this.position.x + this.bigRadius + this.velocity.x < canvas.width) {
                this.position.x += this.velocity.x
           }
-          if (this.position.y - this.bigRadius / 2 + this.velocity.y > 0 && this.position.y + this.bigRadius / 2 + this.velocity.y < canvas.height) {
+          if (this.position.y - this.bigRadius + this.velocity.y > 0 && this.position.y + this.bigRadius + this.velocity.y < canvas.height) {
                this.position.y += this.velocity.y
           }
-          
-          // TODO - Detect direction changes and draw rectangles
+
+          // TODO - Detect direction changes and draw rectangles to reduce memory usage and computation time
           const actualPos = JSON.stringify(this.position)
-          if (!this.path.includes(actualPos)){
+          if (!this.path.includes(actualPos)) {
                this.path.push(actualPos)
           }
           this.draw()
@@ -59,7 +104,10 @@ const player = new Player()
 function animate() {
      requestAnimationFrame(animate)
      c.clearRect(0, 0, canvas.width, canvas.height)
-     player.update()
+     drawMap()
+     if (started) {
+          player.update()
+     }
 }
 
 animate()
@@ -69,27 +117,34 @@ addEventListener('keydown', ({
 }) => {
      switch (key) {
           case 'ArrowLeft':
-               if (player.velocity.x == 0){
-                    player.velocity.x = -1
+               if (player.velocity.x == 0) {
+                    player.velocity.x = -player.speed
                     player.velocity.y = 0
                }
                break
           case 'ArrowUp':
-               if (player.velocity.y == 0){
-                    player.velocity.y = -1
+               if (player.velocity.y == 0) {
+                    player.velocity.y = -player.speed
                     player.velocity.x = 0
                }
                break
           case 'ArrowRight':
-               if (player.velocity.x == 0){
-                    player.velocity.x = 1
+               if (player.velocity.x == 0) {
+                    player.velocity.x = player.speed
                     player.velocity.y = 0
                }
                break
           case 'ArrowDown':
-               if (player.velocity.y == 0){
-                    player.velocity.y = 1
+               if (player.velocity.y == 0) {
+                    player.velocity.y = player.speed
                     player.velocity.x = 0
+               }
+               break
+          case ' ':
+               if (!started) {
+                    started = true
+                    const title = document.querySelector('.canvas-container h3')
+                    title.style.opacity = 0
                }
                break
 
